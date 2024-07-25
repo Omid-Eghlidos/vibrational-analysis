5e25c91642da42d91541946adbc22e1848aeab8f
omid_laptop <oeghlido@asu.edu>
2024-07-25 00:32:43 -0500

    Modified main.m to account for changes made in the previous commit.

--- main.m
+++ main.m
@@ -5,11 +5,11 @@
 % Initializes variables, manages dependencies, and executes main functions.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
 %% Adjust Default Settings
 % Clear workspace, command window, and close all figures
 clear; clc; close all; 
 
+disp('* Start vibrational analysis of finite elements model of structures...');
 t_start = tic;
 % Variable format and warning settings
 format long;
@@ -22,9 +22,9 @@ set(groot, 'defaultLegendInterpreter', 'latex');
 
 %% Initialization
 
-disp('* Start vibrational analysis of finite elements model of structures...');
+disp('-- Initializing...');
 % Add project folder containig files for model parameters and load them
-addpath('Inputs', "Library");
+addpath('Inputs', 'Library');
 params = load('parameters.mat');
 % Add number of low frequency modes to consider for plotting mode shapes
 params.low_modes = 6;
@@ -33,49 +33,23 @@ params.num_w = 150;
 % Modes to plot
 params.plotting_modes = [10, 15];
 
-% Initializing a parallel pool (used for computing impedance matrix response)
-if isempty(gcp('nocreate'))
-    parpool;
-end
-
-% Initializing class instances for the model and each method
-disp('-- Initializing...');
-% Finite elements model (FEM)
-FEM = getObjectInstances("FEM", params); 
-% Modal expansion method (Modal)
-Modal = getObjectInstances("Modal", params);
-% Impedance matrix method (Impedance) 
-Impedance = getObjectInstances("Impedance", params);
-% Plotting (Plot) 
-Plot = getObjectInstances("Plot", params);
-
-
 %% Modeling and Analysis
 
+disp('-- Starting analysis')
+% Finite elements model (FEM)
+FEM = finiteElementsMethod(params);
 % Modal expansion method
-% Compute the damped steady-state and transient responses for harmonic excitations
-Modal = Modal.computeForcedHarmonicResponse(params, FEM);
-% Compute the damped free response with final value of forced response as its initial condition
-Modal = Modal.computeDampedFreeResponse(params);
-
+%Modal = modalExpansionMethod(params, FEM);
 % Impedance matrix method
-% Compute the damped steady-state response using the impedance matrix method
-Impedance = Impedance.computeForcedHarmonicResponse(params, FEM);
+%Impedance = impedanceMatrixMethod(params, FEM, Modal);
+% Craig-Bampton method
+CB = craigBamptonMethod(FEM);
+% Plotting
+%plottingMethods(params, FEM, Modal)
 
 
-%% Plotting
-
-% For generating 3D animation of vibrations pass "true" as the second argument 
-% e.g., Plot.modalExapnsion(Modal, true)
-Plot.modalExpansion(Modal)
-Plot.dampedSteadyStateResponse(Modal);
-Plot.dampedTransientResponse(Modal);
-Plot.dampedForcedResponse(Modal);
-Plot.dampedFreeResponse(Modal);
-Plot.impedanceMatrixResponse(Impedance);
-Plot.compareModalAndImpedanceResults(Modal, Impedance);
-
+%% Running statistics
 % Calculate and display the execution time
 delta_t = seconds(toc(t_start));
 delta_t.Format = 'hh:mm:ss.SSS';
-fprintf('* Successfully completed the execution in %s *\n', char(delta_t));
+fprintf('* Execution completed in %s *\n', char(delta_t));
